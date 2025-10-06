76511ab906453771fe703fa196a648f253c6c960
Philipp Dyck <pdyck@posteo.de>
2025-10-06 13:11:25 +0200

    style: formatting

--- plopslop/src/hello.test.ts
+++ plopslop/src/hello.test.ts
@@ -1,7 +1,7 @@
 import { helloWorld } from "./hello.js";
 
 describe("helloWorld", () => {
-	it("should return 'Hello World'", () => {
-		expect(helloWorld()).toBe("Hello World");
-	});
+  it("should return 'Hello World'", () => {
+    expect(helloWorld()).toBe("Hello World");
+  });
 });
