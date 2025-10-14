9178e6fbb8cce3fa264491c03f70b5fa11ec827c
Philipp Dyck <pdyck@posteo.de>
2025-10-14 14:55:33 +0200

    refactor: remove old package

--- plopslop/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { eventEmitter } from "./event-emitter.js";
-export { createPubSub } from "./pubsub.js";
-export { redis } from "./redis.js";
-export * from "./types.js";
-export * from "./util.js";
